559b4a4cba9da66b1a76eee7f46b1ac2018c74fa
jtracos <69091333+jtracos@users.noreply.github.com>
2021-12-16 19:15:52 -0600

    Update amplitudes_registros.py

--- amplitudes_registros.py
+++ amplitudes_registros.py
@@ -38,19 +38,30 @@ def obtener_amplitud(dfs):
 
 
 def obtener_amplitudes(path, pattern):
+    """funcion principal para generar los archivos
+    path: string que define la ruta a la carpeta donde se alojan los archivos
+    pattern: string usado para buscar los archivos de donde se extraen los
+    nombres de las estaciones y eventos
+    """
     os.chdir(path)
     files_comp = glob.glob('*e-*.txt')
     files = glob.glob('*.txt')
     metadata = obtener_estacion_evento(files_comp)
+    print(metadata)
     componentes = obtener_archivos_componentes(files, metadata)
     for _, comp in enumerate(componentes):#enumerate: para obtener el metadata correspondiente y nombrar el archivo
+        print(comp)
         dfs = []
         for e,f in enumerate(comp):
             df_ = pd.read_csv(f, skiprows=1, sep='\s+',header=None, names=['freq',f'amp{e}']) 
             dfs.append(df_)
         df_temp = obtener_amplitud(dfs)
+        df_temp['freq'] = df_temp['freq'].apply(str)
+        with open(componentes[_][0]) as f:
+          fl = next(f)
         nm = metadata[_][0] + '-' + metadata[_][1]#se guardan con la nomenclatura 'estacion'-'evento's.txt
-        df_temp.to_csv(nm+'s.txt',sep=' ', float_format='%.4E')
+        with open(nm+'s.txt', mode = 'w') as f:
+          f.write(fl+df_temp.to_string(header=False, float_format='%.4E', index=False))
 
 if __main__ == '__main__':
 
